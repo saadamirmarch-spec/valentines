270c60371f08a273ac9587ce2220e9f83911c833
saadamirmarch-spec <saadamirmarch@gmail.com>
2026-02-01 21:49:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
     validateConfig();
 
     // Set texts from config
-    document.getElementById('valentineTitle').textContent = "My Princess";
+    document.getElementById('valentineTitle').textContent = `${config.valentineName}, my princess...`;
     
     // Set first question texts
     document.getElementById('question1Text').textContent = config.questions.first.text;
