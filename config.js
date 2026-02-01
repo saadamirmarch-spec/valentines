f550bc8c40d0178bbf972712741fb95692a10d03
saadamirmarch-spec <saadamirmarch@gmail.com>
2026-02-01 20:52:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "SOO, Will you be my Valentine? 🌹", // The big question!
+            text: "Will you be my Valentine? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
